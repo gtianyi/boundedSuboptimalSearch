8d1c403f7ebe0ea8203d09cdfa6e8f090b3d9a17
Tianyi Gu <2008tygu@gmail.com>
2021-01-30 21:10:33 -0500

    cpp - rbtree work without interval data retrieve

--- src/utility/test/RBTree-test.cpp
+++ src/utility/test/RBTree-test.cpp
@@ -1,12 +1,51 @@
+#include "../../BoundedSuboptimalSearch.hpp"
+#include "../../domain/SlidingTilePuzzle.h"
 #include "../RBTree.h"
 #include <gtest/gtest.h>
 
 namespace test {
+typedef BoundedSuboptimalSearch<SlidingTilePuzzle>::Node SearchNode;
+
 TEST(RBTree, insertion)
 {
-    RBTree bst;
-    bst.insert(8);
-    EXPECT_EQ(bst.getRoot()->data, 8);
+    SearchNode* cur =
+      new SearchNode(8, 0, 0, 0, 0, 0, SlidingTilePuzzle::State(), NULL);
+    RBTree<SearchNode*> bst(SearchNode::compareNodesF);
+    bst.insert(cur);
+    bst.prettyPrint();
+    // EXPECT_EQ(bst.getRoot()->data, 8);
+}
+
+TEST(RBTree, print)
+{
+    RBTree<SearchNode*> bst(SearchNode::compareNodesF);
+    SearchNode*         cur8 =
+      new SearchNode(8, 0, 0, 0, 0, 0, SlidingTilePuzzle::State(), NULL);
+    SearchNode* cur18 =
+      new SearchNode(18, 0, 0, 0, 0, 0, SlidingTilePuzzle::State(), NULL);
+    SearchNode* cur5 =
+      new SearchNode(5, 0, 0, 0, 0, 0, SlidingTilePuzzle::State(), NULL);
+    SearchNode* cur15 =
+      new SearchNode(15, 0, 0, 0, 0, 0, SlidingTilePuzzle::State(), NULL);
+    SearchNode* cur17 =
+      new SearchNode(17, 0, 0, 0, 0, 0, SlidingTilePuzzle::State(), NULL);
+    SearchNode* cur25 =
+      new SearchNode(25, 0, 0, 0, 0, 0, SlidingTilePuzzle::State(), NULL);
+    SearchNode* cur40 =
+      new SearchNode(40, 0, 0, 0, 0, 0, SlidingTilePuzzle::State(), NULL);
+    SearchNode* cur80 =
+      new SearchNode(80, 0, 0, 0, 0, 0, SlidingTilePuzzle::State(), NULL);
+
+    bst.insert(cur8);
+    bst.insert(cur18);
+    bst.insert(cur5);
+    bst.insert(cur15);
+    bst.insert(cur17);
+    bst.insert(cur25);
+    bst.insert(cur40);
+    bst.insert(cur80);
+    bst.deleteNode(cur25);
+    bst.prettyPrint();
 }
 } // namespace test
 

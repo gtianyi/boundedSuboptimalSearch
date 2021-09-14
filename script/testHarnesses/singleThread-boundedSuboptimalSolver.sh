1d82b72febd87d1bf925db88e1c9ada7a17260b1
Tianyi Gu <2008tygu@gmail.com>
2021-09-14 11:48:34 -0400

    bash - run smhastar

--- script/testHarnesses/singleThread-boundedSuboptimalSolver.sh
+++ script/testHarnesses/singleThread-boundedSuboptimalSolver.sh
@@ -27,8 +27,8 @@ first=1
 # The number of instances to test on
 n_of_i=1
 
-#domain=("tile" "pancake" "racetrack" "vacuumworld")
-domain=("tile")
+domain=("tile" "pancake" "racetrack" "vacuumworld")
+#domain=("tile")
 subdomain=()
 #subdomainTile=("uniform" "heavy" "inverse" "heavy-easy" "reverse-easy" "sqrt")
 subdomainTile=("uniform" "heavy" "inverse")
@@ -38,8 +38,8 @@ subdomainPancake=("regular" "heavy")
 subdomainVacuumworld=("uniform" "heavy-easy")
 #subdomainVacuumworld=("heavy-easy")
 #subdomainVacuumworld=("heavy")
-subdomainRacetrack=("den520d" "ost003d")
-#subdomainRacetrack=("barto-bigger" "den520d" "ost003d")
+#subdomainRacetrack=("den520d" "ost003d")
+subdomainRacetrack=("barto-bigger" "den520d" "ost003d")
 
 heuristicTypes=("NA")
 #heuristicTypePancake=("gap" "gapm2")
@@ -65,7 +65,7 @@ sizeOfSumHeavyPancake="10"
 #bssSolvers=("eesdoylew" "ees" "ees95" "wastar" "dxes" "dps" "roundrobin1" "roundrobin8" "roundrobind1" "roundrobind8")
 #bssSolvers=("eesli" "speedy" "eesdoylew" "ees95doylew" "ees" "roundrobind1" "ees95" "wastar" "dxes" "dps" "roundrobin1")
 #bssSolvers=("roundrobin1" "bfsonp" "dxes")
-bssSolvers=("dpsroundrobin")
+bssSolvers=("smhastar")
 boundPercents=()
 #boundPercentsA=(60 80 100 120 140 160 180 200 220 240 260 280 300 400 500 600 800 1000 1300 2000 3000)
 #boundPercentsA=(1.1 1.2 1.5 2.0 5.0 10)
